a1de5c20cc52d68883dc04543cfc2fd6eb9783cb
Andre W <10945277+alfwro13@users.noreply.github.com>
2023-03-27 15:24:54 +0100

    Update const.py

--- custom_components/givenergy_local/const.py
+++ custom_components/givenergy_local/const.py
@@ -6,6 +6,9 @@ from logging import Logger, getLogger
 DOMAIN = "givenergy_local"
 LOGGER: Logger = getLogger(__package__)
 
+CONF_UPDATE_INTERVAL = "update_interval"
+DEFAULT_UPDATE_INTERVAL = 30 # seconds
+
 CONF_HOST = "host"
 CONF_NUM_BATTERIES = "num_batteries"
 
